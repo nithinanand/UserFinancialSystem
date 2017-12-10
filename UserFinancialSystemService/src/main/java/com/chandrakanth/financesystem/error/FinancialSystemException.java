71993cd090a5aa79fa55d469f8e9d325ff4fd911
ChandrakanthC <chandrakanthcandikattu@gmail.com>
2017-12-10 17:31:27 +0530

    Customized Exception Handler added

---
+++ UserFinancialSystemService/src/main/java/com/chandrakanth/financesystem/error/FinancialSystemException.java
@@ -0,0 +1,23 @@
+package com.chandrakanth.financesystem.error;
+
+public class FinancialSystemException extends Exception{
+
+	public  FinancialSystemException() {
+		super();
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public FinancialSystemException(Exception e) {
+		
+	}
+	
+	
+	
+}
