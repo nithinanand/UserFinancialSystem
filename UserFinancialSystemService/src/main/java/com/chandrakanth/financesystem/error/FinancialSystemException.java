103b46fbc5f0c60c8c79806ac09077e5ca857f9c
ChandrakanthC <chandrakanthcandikattu@gmail.com>
2017-12-10 18:56:30 +0530

    Modifed Exception Handler

--- UserFinancialSystemService/src/main/java/com/chandrakanth/financesystem/error/FinancialSystemException.java
+++ UserFinancialSystemService/src/main/java/com/chandrakanth/financesystem/error/FinancialSystemException.java
@@ -1,23 +1,49 @@
 package com.chandrakanth.financesystem.error;
 
-public class FinancialSystemException extends Exception{
+import java.util.List;
 
-	public  FinancialSystemException() {
+public class FinancialSystemException extends Exception {
+
+	public FinancialSystemException() {
 		super();
 	}
-	
-	
-	
+
+	private List<Exception> exceptionList = null;
+
+	private List<String> errorMessageList = null;
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
 	public FinancialSystemException(Exception e) {
-		
+
 	}
-	
-	
-	
+
+	public List<Exception> getExceptionList() {
+		return exceptionList;
+	}
+
+	public void setExceptionList(List<Exception> exceptionList) {
+		this.exceptionList = exceptionList;
+	}
+
+	public List<String> getErrorMessageList() {
+		return errorMessageList;
+	}
+
+	public void setErrorMessageList(List<String> errorMessageList) {
+		this.errorMessageList = errorMessageList;
+	}
+
+	public FinancialSystemException(List<String> exceptionMessageList) {
+		errorMessageList = exceptionMessageList;
+	}
+
+	public FinancialSystemException(List<Exception> exceptionList, List<String> exceptionMessagelist) {
+		errorMessageList = exceptionMessagelist;
+		this.exceptionList = exceptionList;
+	}
+
 }
